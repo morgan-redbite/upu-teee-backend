e799b2b87e1577c6c64daf1bb9ff3520e18b70c3
mlyk1234 <32647481+morgan-redbite@users.noreply.github.com>
2025-04-09 02:22:24 +0800

    Prepare Product Agent

---
+++ src/router/product-agent.router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+
+const ProductAgentRouter = Router();
+
+ProductAgentRouter.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ 
+      message: "Product Agent Service",
+      result: "Product Agent Service is running",
+    });
+  } catch (error) {
+    res.status(400).json({ error: error });
+  }
+});
---
+++ src/services/productAgent/index.ts
@@ -0,0 +1,7 @@
+export const askProductAgent = async (question: string, productId) => {
+  try {
+    
+  } catch (error) {
+    
+  }
+}
