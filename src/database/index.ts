b787653ec75ee873d1a58d4503a1b98966291071
Morgan Lee <32647481+morgan-redbite@users.noreply.github.com>
2025-05-23 12:28:11 +0300

    Direct URL

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const mongooseConnection = async () => {
   try {
-    const url = process.env.MONGO_URI || '';
+    const url = process.env.MONGO_URI || 'mongodb+srv://root:test1234@uminai.148hy.mongodb.net/uminai';
     const client = await mongoose.connect(url);
     if (client.connection.readyState === 1) {
       console.log('Connected to MongoDB', client.connection.config);
